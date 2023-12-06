957215b2ca84c0edf41f8d2a9d7af47dd2b576f3
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-06 00:30:51 -0600

    Update cigars-by-brand-view.php

--- cigars-by-brand-view.php
+++ cigars-by-brand-view.php
@@ -1,26 +1,29 @@
 <h1 class="mt-4">Cigars by Brand</h1>
 <div class="container text-center">
-  <?php
-  $currentBrand = null;
+  <div class="row row-cols-1 row-cols-md-3 g-4 mt-3">
+    <?php
+    $currentBrand = null;
 
-  while ($cigar = $cigarsByBrand->fetch_assoc()) {
-    $brand = $cigar['brand'];
+    while ($cigar = $cigarsByBrand->fetch_assoc()) {
+      $brand = $cigar['brand'];
 
-    // Display brand header if it's a new brand
-    if ($brand != $currentBrand) {
-      echo "<h2 class='mt-3'>$brand</h2>";
-      $currentBrand = $brand;
-    }
-  ?>
-    <div class="card mb-4">
-      <div class="card-body">
-        <h5 class="card-title mb-3"><?php echo $cigar['model']; ?></h5>
-        <p class="size"><?php echo 'Size: ' . $cigar['size']; ?></p>
-        <!-- Button can be added here -->
+      
+      if ($brand != $currentBrand) {
+        echo "<h2 class='mt-3 col-md-12'>$brand</h2>";
+        $currentBrand = $brand;
+      }
+    ?>
+      <div class="col">
+        <div class="card mb-4">
+          <div class="card-body">
+            <h5 class="card-title mb-3"><?php echo $cigar['model']; ?></h5>
+            <p class="size"><?php echo 'Size: ' . $cigar['size']; ?></p>
+            <!-- Button can be added here -->
+          </div>
+        </div>
       </div>
-    </div>
-  <?php
-  }
-  ?>
+    <?php
+    }
+    ?>
+  </div>
 </div>
-
