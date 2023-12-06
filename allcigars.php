833c729e784259d78d7bdefc2ec299b4e7444152
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-06 11:38:21 -0600

    Update allcigars.php

--- allcigars.php
+++ allcigars.php
@@ -10,4 +10,32 @@ include "allcigars-view.php";
 ?>
 
 
+<?php
+$pageTitle = "Cigars";
+require_once("util-db.php");
+require_once("allcigars-model.php");
+
+// Handle form submissions for adding and editing cigars
+if (isset $_POST['actionType']) {
+    switch ($_POST['actionType']) {
+        case "Add":
+            if (insertCigar($_POST['cigarBrand'], $_POST['cigarModel'], $_POST['cigarSize'], $_POST['cigarAvailable'], $_POST['cigarPrice'])) {
+                echo '<div class="alert alert-success" role="alert">Cigar added.</div>';
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Error adding cigar.</div>';
+            }
+            break;
+        case "Edit":
+            if (updateCigar($_POST['cid'], $_POST['editCigarBrand'], $_POST['editCigarModel'], $_POST['editCigarSize'], $_POST['editCigarAvailable'], $_POST['editCigarPrice'])) {
+                echo '<div class="alert alert-success" role="alert">Cigar edited.</div>';
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Error editing cigar.</div>';
+            }
+            break;
+    }
+}
+
+$cigars = selectCigars();
+include "allcigars-view.php";
+?>
 
