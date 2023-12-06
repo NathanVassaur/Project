83a4a52958b92fb829c4aff5e9a8eed6c987de8c
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-06 02:36:22 -0600

    Update allcigars-model.php

--- allcigars-model.php
+++ allcigars-model.php
@@ -2,7 +2,7 @@
 function selectCigars() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT cigar_id, brand, model, available, price size FROM `Cigars`");
+        $stmt = $conn->prepare("SELECT cigar_id, brand, model, available, price, size FROM `Cigars`");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
