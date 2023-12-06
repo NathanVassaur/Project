1338ad6edb711f87b22109c157ad84a9f130718a
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-06 01:12:43 -0600

    Update cigars-by-size-model.php

--- cigars-by-size-model.php
+++ cigars-by-size-model.php
@@ -2,26 +2,14 @@
 function selectCigarsBySize() {
     try {
         $conn = get_db_connection();
-
-     
-        $sizeCountQuery = "SELECT size, COUNT(*) as count FROM `Cigars` GROUP BY size ORDER BY count DESC";
-        $sizeCountResult = $conn->query($sizeCountQuery);
-        $sizeCounts = array();
-
-        while ($row = $sizeCountResult->fetch_assoc()) {
-            $sizeCounts[$row['size']] = $row['count'];
-        }
-
-        $cigarsQuery = "SELECT cigar_id, brand, model, size FROM `Cigars` ORDER BY FIELD(size, " . implode(", ", array_keys($sizeCounts)) . ") DESC";
-        $cigarsResult = $conn->query($cigarsQuery);
-
+        $stmt = $conn->prepare("SELECT cigar_id, brand, model, size FROM `Cigars` ORDER BY size");
+        $stmt->execute();
+        $result = $stmt->get_result();
         $conn->close();
-
-        return $cigarsBySize;
+        return $result;
     } catch (Exception $e) {
         $conn->close();
         throw $e;
     }
 }
 ?>
-
