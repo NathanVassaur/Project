5bd9c6a3963d9417f002f1b62b08dafccc878fc4
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-06 02:33:34 -0600

    Update allcigars-view.php

--- allcigars-view.php
+++ allcigars-view.php
@@ -19,7 +19,7 @@
             <h5 class="card-title mb-3"><?php echo $cigar['model']; ?></h5>
             <p class="brand mb-2"><?php echo 'Brand: ' . $cigar['brand']; ?></p>
             <p class="size"><?php echo 'Size: ' . $cigar['size']; ?></p>
-            <p class="size"><?php echo 'In Stock: ' . $cigar['available']; ?></p>
+            <p class="size"><?php echo 'In Stock: ' . $cigar['available']; . 'Price: $' . $cigar['price'] '.00'?></p>
             <!-- Button can be added here -->
           </div>
         </div>
