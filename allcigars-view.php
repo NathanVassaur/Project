11cfcf16beec24d684181c202e6c8eb6c8d1c2c0
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-06 00:03:45 -0600

    Update allcigars-view.php

--- allcigars-view.php
+++ allcigars-view.php
@@ -3,16 +3,16 @@
   <div class="row row-cols-1 row-cols-md-3 g-4 mt-3">
     <?php
     while ($cigar = $cigars->fetch_assoc()) {
-      $imagePath = "https://github.com/NathanVassaur/Project/blob/main/images/" . $cigar['cigar_id'] . ".png?raw=true";
+      $imagePath = "https://raw.githubusercontent.com/NathanVassaur/Project/main/images/" . $cigar['cigar_id'] . ".png";
     ?>
       <div class="col">
         <div class="card mb-4">
           <?php if (imageExists($imagePath)) { ?>
             <img src="<?php echo $imagePath; ?>" class="card-img-top mx-auto d-block" alt="Cigar Image" style="max-width: 150px;">
           <?php } else { ?>
-            <! placeholder -->
+            <!-- Placeholder image or alternative content -->
             <div class="text-center">
-              
+              <p>No Image Available</p>
             </div>
           <?php } ?>
           <div class="card-body">
@@ -25,7 +25,7 @@
       </div>
     <?php
     }
-    
+
     function imageExists($url) {
         $headers = get_headers($url);
         return stripos($headers[0], "200 OK") ? true : false;
