ff62ef431fb0ef9a5ec87189dba657b06b5afcc6
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-06 00:34:30 -0600

    Update cigars-by-brand-view.php

--- cigars-by-brand-view.php
+++ cigars-by-brand-view.php
@@ -7,7 +7,7 @@
     while ($cigar = $cigarsByBrand->fetch_assoc()) {
       $brand = $cigar['brand'];
 
-      
+      // Display brand header if it's a new brand
       if ($brand != $currentBrand) {
         echo "<h2 class='mt-3 col-md-12'>$brand</h2>";
         $currentBrand = $brand;
@@ -15,6 +15,14 @@
     ?>
       <div class="col">
         <div class="card mb-4">
+          <?php if (imageExists($imagePath)) { ?>
+            <img src="<?php echo $imagePath; ?>" class="card-img-top mx-auto d-block" alt="Cigar Image" style="max-width: 150px;">
+          <?php } else { ?>
+            <!-- Placeholder image or alternative content -->
+            <div class="text-center">
+              <p>No Image Available</p>
+            </div>
+          <?php } ?>
           <div class="card-body">
             <h5 class="card-title mb-3"><?php echo $cigar['model']; ?></h5>
             <p class="size"><?php echo 'Size: ' . $cigar['size']; ?></p>
