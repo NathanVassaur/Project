e5bac7a9fe2d2f9c87de94ec17160cd758e8d146
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-06 12:49:58 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -6,15 +6,6 @@
     <title><?=$pageTitle?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
-    <script>
-        var passwordProtected = true;
-
-        if (passwordProtected) {
-            if (!window.location.pathname.includes("password.php")) {
-                window.location.href = 'password.php';
-            }
-        }
-    </script>
 <body>
     <div class="container">
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
@@ -29,7 +20,7 @@
                             <a class="nav-link active" aria-current="page" href="/">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="allcigars.php">Admin</a>
+                            <a class="nav-link active" aria-current="page" href="#" onclick="showPasswordModal()">Admin</a>
                         </li>
                         
                         <li class="nav-item dropdown">
