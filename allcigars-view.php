ab9d75d17ec2bb818d99354ec643c205f15a698e
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-06 00:49:25 -0600

    Update allcigars-view.php

--- allcigars-view.php
+++ allcigars-view.php
@@ -6,16 +6,16 @@
       $imagePath = "https://raw.githubusercontent.com/NathanVassaur/Project/main/images/" . $cigar['cigar_id'] . ".png";
     ?>
       <div class="col">
-        <div class="card mb-4">
+        <div class="card mb-4 h-100">
           <?php if (imageExists($imagePath)) { ?>
-            <img src="<?php echo $imagePath; ?>" class="card-img-top mx-auto d-block" alt="Cigar Image" style="max-width: 150px;">
+            <img src="<?php echo $imagePath; ?>" class="card-img-top mx-auto d-block" alt="Cigar Image" style="max-width: 150px; height: 150px;">
           <?php } else { ?>
             <!-- Placeholder image or alternative content -->
-            <div class="text-center">
+            <div class="text-center" style="height: 150px;">
               <p>No Image Available</p>
             </div>
           <?php } ?>
-          <div class="card-body">
+          <div class="card-body" style="height: 150px;">
             <h5 class="card-title mb-3"><?php echo $cigar['model']; ?></h5>
             <p class="brand mb-2"><?php echo 'Brand: ' . $cigar['brand']; ?></p>
             <p class="size"><?php echo 'Size: ' . $cigar['size']; ?></p>
@@ -27,8 +27,12 @@
     }
 
     function imageExists($url) {
+      if (!empty($url)) {
         $headers = get_headers($url);
         return stripos($headers[0], "200 OK") ? true : false;
+      } else {
+        return false;
+      }
     }
     ?>
   </div>
