91cd2c6430b23292210a9f81e8f964506e4e1737
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-06 00:27:47 -0600

    Update cigars-by-brand-model.php

--- cigars-by-brand-model.php
+++ cigars-by-brand-model.php
@@ -1,1 +1,16 @@
 
+<?php
+function selectCigarsByBrand() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT brand, model, size FROM `Cigars` ORDER BY brand, model");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
