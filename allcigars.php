09d385d6e27a090ec32f2ba2c9a1cd43c6d5e6d9
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-05 21:49:45 -0600

    Update and rename homepage.php to allcigars.php

---
+++ allcigars.php
@@ -0,0 +1,15 @@
+<?php
+$pageTitle = "Home";
+<?php
+require_once("util-db.php");
+require_once("model-allcigars.php");
+
+$pageTitle = "Cigars";
+include "view-header.php";
+$cigars = selectCigars();
+include "view-allcigars.php";
+include "view-footer.php";
+?>
+
+
+
