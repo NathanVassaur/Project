3fcd4fd9fcb8688df101f9588c7127652861ceb5
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-06 00:51:43 -0600

    Update cigars-by-size.php

--- cigars-by-size.php
+++ cigars-by-size.php
@@ -1,1 +1,9 @@
+<?php
+$pageTitle = "Cigars by Size";
+require_once("util-db.php");
+require_once("cigars-by-size-model.php");
+
+$cigarsBySize = selectCigarsBySize();
+include "cigars-by-size-view.php";
+?>
 
