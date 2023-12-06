355c5d33668a908c382fa9864184c118fb669db4
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-06 11:21:22 -0600

    Create allcigars-newform-view.php

---
+++ allcigars-newform-view.php
@@ -0,0 +1,45 @@
+
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newCigarModal">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+      <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+    </svg>
+</button>
+
+
+<div class="modal fade" id="newCigarModal" tabindex="-1" aria-labelledby="newCigarModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="newCigarModalLabel">Add New Cigar</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form method="post" action="">
+                    <div class="mb-3">
+                        <label for="cigarModel" class="form-label">Cigar Model</label>
+                        <input type="text" class="form-control" id="cigarModel" name="cigarModel" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="cigarBrand" class="form-label">Cigar Brand</label>
+                        <input type="text" class="form-control" id="cigarBrand" name="cigarBrand" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="cigarSize" class="form-label">Cigar Size</label>
+                        <input type="text" class="form-control" id="cigarSize" name="cigarSize" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="cigarAvailable" class="form-label">Inventory to Add</label>
+                        <input type="text" class="form-control" id="cigarAvailable" name="cigarAvailable" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="cigarPrice" class="form-label">Price</label>
+                        <input type="text" class="form-control" id="cigarPrice" name="cigarPrice" required>
+                    </div>
+                    <input type="hidden" name="actionType" value="Add">
+                    <button type="submit" class="btn btn-primary">Add Cigar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
