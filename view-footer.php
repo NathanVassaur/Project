59bda91d107407a54392d6efa39678c10e22cecb
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-06 12:56:31 -0600

    Update view-footer.php

--- view-footer.php
+++ view-footer.php
@@ -1,4 +1,11 @@
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<?php include "password.php"; ?>
+  <script>
+        function showPasswordModal() {
+            var passwordModal = new bootstrap.Modal(document.getElementById('passwordModal'));
+        passwordModal.show();
+        }
+    </script>
 </body>
 </html>
