5fd5aa3e00810263baa5ae4594be5152248a50c1
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-06 00:27:28 -0600

    Update cigars-by-brand.php

--- cigars-by-brand.php
+++ cigars-by-brand.php
@@ -1,1 +1,9 @@
+<?php
+$pageTitle = "Cigars by Brand";
+require_once("util-db.php");
+require_once("cigars-by-brand-model.php");
+
+$cigarsByBrand = selectCigarsByBrand();
+include "cigars-by-brand-view.php";
+?>
 
