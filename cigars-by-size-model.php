09db776d02490236173ad9dd8c87a9205a4700e7
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-06 01:25:30 -0600

    Update cigars-by-size-model.php

--- cigars-by-size-model.php
+++ cigars-by-size-model.php
@@ -1,15 +1,42 @@
-<?php
-function selectCigarsBySize() {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT size, COUNT(*) as size_count FROM `Cigars` GROUP BY size ORDER BY size_count DESC, size");
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $conn->close();
-        return $result;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
+<?php include "view-header.php"; ?>
+
+<h1 class="mt-4">Cigars by Brand</h1>
+<div class="container text-center">
+  <div class="row row-cols-1 row-cols-md-3 g-4 mt-3">
+    <?php
+    $currentBrand = null;
+
+    while ($cigar = $cigarsByBrand->fetch_assoc()) {
+      $brand = $cigar['brand'];
+
+      // Display brand header if it's a new brand
+      if ($brand != $currentBrand) {
+        echo "<h2 class='mt-3 col-md-12'>$brand</h2>";
+        $currentBrand = $brand;
+      }
+    ?>
+      <div class="col">
+        <div class="card mb-4">
+          <?php if (imageExists($imagePath)) { ?>
+            <img src="<?php echo $imagePath; ?>" class="card-img-top mx-auto d-block" alt="Cigar Image" style="max-width: 150px;">
+          <?php } else { ?>
+            <!-- Placeholder image or alternative content -->
+            <div class="text-center">
+              <p>No Image Available</p>
+            </div>
+          <?php } ?>
+          <div class="card-body">
+            <h5 class="card-title mb-3"><?php echo $cigar['model']; ?></h5>
+            <p class="size"><?php echo 'Size: ' . $cigar['size']; ?></p>
+            <!-- Button can be added here -->
+          </div>
+        </div>
+      </div>
+    <?php
     }
-}
-?>
+    ?>
+  </div>
+</div>
+
+
+<?php include "view-footer.php"; ?>
