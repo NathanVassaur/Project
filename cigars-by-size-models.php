f7ec66873bde408296a87079ffcb5ee20ace18a1
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-06 00:52:12 -0600

    Create cigars-by-size-models.php

---
+++ cigars-by-size-models.php
@@ -0,0 +1,15 @@
+<?php
+function selectCigarsBySize() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT cigar_id, brand, model, size FROM `Cigars` ORDER BY size");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
