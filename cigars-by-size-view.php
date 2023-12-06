38b6733a54c527253d5479dd93ec19f0897d812e
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-06 01:26:07 -0600

    Update cigars-by-size-view.php

--- cigars-by-size-view.php
+++ cigars-by-size-view.php
@@ -1,57 +1,41 @@
-<?php
-include "view-header.php"
-  ?>
+<?php include "view-header.php"; ?>
 
-<h1 class="mt-4">Cigars by Size</h1>
+<h1 class="mt-4">Cigars by Brand</h1>
 <div class="container text-center">
-  <div class="row mt-3">
+  <div class="row row-cols-1 row-cols-md-3 g-4 mt-3">
     <?php
-    while ($sizeGroup = $cigarsBySize->fetch_assoc()) {
-      $size = $sizeGroup['size'];
-      $sizeCount = $sizeGroup['size_count'];
+    $currentBrand = null;
 
-      echo "<h2 class='mt-3 col-md-12'>$size (Count: $sizeCount)</h2>";
+    while ($cigar = $cigarsByBrand->fetch_assoc()) {
+      $brand = $cigar['brand'];
 
-      
-      $cigarsForSizeQuery = "SELECT cigar_id, brand, model FROM `Cigars` WHERE size = ? ORDER BY brand";
-      $cigarsForSizeStmt = $conn->prepare($cigarsForSizeQuery);
-      $cigarsForSizeStmt->bind_param("s", $size);
-      $cigarsForSizeStmt->execute();
-      $cigarsForSizeResult = $cigarsForSizeStmt->get_result();
-
-      while ($cigar = $cigarsForSizeResult->fetch_assoc()) {
-        $imagePath = "https://raw.githubusercontent.com/NathanVassaur/Project/main/images/" . $cigar['cigar_id'] . ".png";
+      if ($brand != $currentBrand) {
+        echo "<h2 class='mt-3 col-md-12'>$brand</h2>";
+        $currentBrand = $brand;
+      }
     ?>
-        <div class="col">
-          <div class="card mb-4 h-100">
-            <?php if (imageExists($imagePath)) { ?>
-              <img src="<?php echo $imagePath; ?>" class="card-img-top mx-auto d-block" alt="Cigar Image" style="max-width: 150px; height: 150px;">
-            <?php } else { ?>
-              
-              <div class="text-center" style="height: 150px;">
-                <p>No Image Available</p>
-              </div>
-            <?php } ?>
-            <div class="card-body">
-              <h5 class="card-title mb-3"><?php echo $cigar['model']; ?></h5>
-              <p class="brand mb-2"><?php echo 'Brand: ' . $cigar['brand']; ?></p>
-              
+      <div class="col">
+        <div class="card mb-4">
+          <?php if (imageExists($imagePath)) { ?>
+            <img src="<?php echo $imagePath; ?>" class="card-img-top mx-auto d-block" alt="Cigar Image" style="max-width: 150px;">
+          <?php } else { ?>
+         
+            <div class="text-center">
+              <p>No Image Available</p>
             </div>
+          <?php } ?>
+          <div class="card-body">
+            <h5 class="card-title mb-3"><?php echo $cigar['model']; ?></h5>
+            <p class="size"><?php echo 'Size: ' . $cigar['size']; ?></p>
+          
           </div>
         </div>
+      </div>
     <?php
-      }
     }
     ?>
   </div>
 </div>
 
-<?php
-function imageExists($url) {
-  $headers = get_headers($url);
-  return stripos($headers[0], "200 OK") ? true : false;
-}
-?>
-<?php
-include "view-footer.php"
-?>
+
+<?php include "view-footer.php"; ?>
