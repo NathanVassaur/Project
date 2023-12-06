94f8a31d2f952a375cb97f4b824908b671c4aec1
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-05 23:10:20 -0600

    Update view-allcigars.php

--- view-allcigars.php
+++ view-allcigars.php
@@ -9,7 +9,7 @@
           <!-- Image can be added here -->
           <div class="card-body">
             <h5 class="card-title"><?php echo $cigar['model']; ?></h5>
-            <p class="brand"><?php echo 'Brand: $cigar['brand']; ?></p>
+            <p class="brand"><?php echo 'Brand:' $cigar['brand']; ?></p>
             <p class="size"><?php echo 'Size' $cigar['size']; ?></p>
             <!-- Button can be added here -->
           </div>
