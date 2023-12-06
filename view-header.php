3092bb86c935cd7f993c203ee76705ef170e255a
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-06 12:46:08 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -6,6 +6,15 @@
     <title><?=$pageTitle?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
+    <script>
+        var passwordProtected = true;
+
+        if (passwordProtected) {
+            if (!window.location.pathname.includes("password.php")) {
+                window.location.href = 'password.php';
+            }
+        }
+    </script>
 <body>
     <div class="container">
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
