e93bce7200e88e82c11f2ce06a9ded113a59cc6e
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-06 01:00:54 -0600

    Update cigars-by-size-model.php

--- cigars-by-size-model.php
+++ cigars-by-size-model.php
@@ -2,7 +2,7 @@
 function selectCigarsBySize() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT cigar_id, brand, model, size FROM `Cigars` ORDER BY size");
+        $stmt = $conn->prepare("SELECT cigar_id, brand, model, size FROM `Cigars` ORDER BY size DESC"); 
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
