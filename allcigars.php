46492c2c9b0ee6b9868a4b11e92a4a548858db91
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-05 23:16:01 -0600

    Update allcigars.php

--- allcigars.php
+++ allcigars.php
@@ -1,13 +1,12 @@
 <?php
-$pageTitle = "Home";
+$pageTitle = "Cigars";
 require_once("util-db.php");
 require_once("model-allcigars.php");
 
-$pageTitle = "Cigars";
-include "view-header.php";
+
 $cigars = selectCigars();
 include "view-allcigars.php";
-include "view-footer.php";
+
 ?>
 
 
