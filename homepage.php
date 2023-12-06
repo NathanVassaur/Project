5eda48c1589e60aa61c2b1ca71d7ceb4a19d86f1
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-05 19:18:59 -0600

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -1,1 +1,19 @@
+<?php
+$pageTitle = "Home";
+include "view-header.php";
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        Column
+      </div>
+      <div class="col">
+        Column
+      </div>
+      <div class="col">
+        Column
+      </div>
+    </div>
+  </div>
 
+include "view-footer.php";
+?>
