3a25a5feb474a873c7e063c75a7f5c3f2c6e7959
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-15 19:25:08 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -32,20 +32,8 @@
                             <li><a class="dropdown-item" href="cigars-by-size.php">By Size</a></li>
                         
                           </ul>
-                    
+
                         </li>
-                        <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
-                                <ul class="navbar-nav ms-auto">
-                                    <li class="nav-item">
-                                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newCigarModal">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
-                                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                                                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-                                            </svg>
-                                        </button>
-                                    </li>
-                                </ul>
-                        </div>
                     </ul>
                 </div>
             </div>
