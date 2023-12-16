6ba279f7f8bfde39363c472ac819d439686a92c3
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-15 20:52:44 -0600

    Update signup-newform-view.php

--- signup-newform-view.php
+++ signup-newform-view.php
@@ -1,8 +1,11 @@
-<form method="post" action="">
+<form method="post" action="" onsubmit="return validateForm()">
     <div class="mb-3">
-        <div class="form-floating has-validation">
-            <input type="text" class="form-control" id="email" name="email" placeholder="Email" required>
-            <label for="email">Email</label>
+        <div class="input-group has-validation">
+            <span class="input-group-text">@</span>
+            <div class="form-floating">
+                <input type="text" class="form-control" id="email" name="email" placeholder="Email" required>
+                <label for="email">Email</label>
+            </div>
         </div>
     </div>
     <div class="mb-3">
@@ -17,6 +20,32 @@
             <label for="lastName">Last Name</label>
         </div>
     </div>
+    <div class="mb-3">
+        <div class="form-floating">
+            <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
+            <label for="password">Password</label>
+        </div>
+    </div>
+    <div class="mb-3">
+        <div class="form-floating">
+            <input type="password" class="form-control" id="reenterPassword" name="reenterPassword" placeholder="Reenter Password" required>
+            <label for="reenterPassword">Reenter Password</label>
+        </div>
+    </div>
     <input type="hidden" name="actionType" value="Add">
     <button type="submit" class="btn btn-primary">Sign Up</button>
 </form>
+
+<script>
+    function validateForm() {
+        var password = document.getElementById("password").value;
+        var reenterPassword = document.getElementById("reenterPassword").value;
+
+        if (password !== reenterPassword) {
+            alert("Passwords do not match. Please reenter your password.");
+            return false;
+        }
+
+        return true;
+    }
+</script>
