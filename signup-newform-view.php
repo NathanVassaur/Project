53066fdeb20fb1b92a6150471c20d1cb77ba1e2e
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-15 20:29:54 -0600

    Create signup-newform-view.php

---
+++ signup-newform-view.php
@@ -0,0 +1,25 @@
+<form method="post" action="">
+    <div class="mb-3">
+        <div class="input-group has-validation">
+            <span class="input-group-text">@</span>
+            <div class="form-floating">
+                <input type="text" class="form-control" id="email" name="email" placeholder="Email" required>
+                <label for="email">Email</label>
+            </div>
+        </div>
+    </div>
+    <div class="mb-3">
+        <div class="form-floating">
+            <input type="text" class="form-control" id="firstName" name="firstName" placeholder="First Name" required>
+            <label for="firstName">First Name</label>
+        </div>
+    </div>
+    <div class="mb-3">
+        <div class="form-floating">
+            <input type="text" class="form-control" id="lastName" name="lastName" placeholder="Last Name" required>
+            <label for="lastName">Last Name</label>
+        </div>
+    </div>
+    <input type="hidden" name="actionType" value="Add">
+    <button type="submit" class="btn btn-primary">Sign Up</button>
+</form>
