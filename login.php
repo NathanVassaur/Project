914f5f7761ef6d55e9e74600f4e7f38add3bf9c3
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-15 21:55:24 -0600

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Enter Password</h5>
+                <h5 class="modal-title">Login</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
@@ -35,7 +35,7 @@
         if (enteredPassword === correctPassword) {
             alert('Success');
         } else {
-            alert('Incorrect password. Please try again.');
+            alert('Incorrect email or password. Please try again.');
         }
     }
 </script>
