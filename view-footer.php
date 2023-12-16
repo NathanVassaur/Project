821c2e4680a9949127ba7e31b00983785ade5c6c
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-15 22:07:51 -0600

    Update view-footer.php

--- view-footer.php
+++ view-footer.php
@@ -9,33 +9,7 @@
     </script>
 
 <script>
-    function authenticateUser() {
-        var enteredEmail = document.getElementById('emailInput').value;
-        var enteredPassword = document.getElementById('passwordInput').value;
-
-        fetch('login-validation.php', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: new URLSearchParams({
-                'email': enteredEmail,
-                'password': enteredPassword,
-            }),
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.status === 'success') {
-                alert('Success');
-                // Redirect to a protected page or perform other actions upon successful login
-            } else {
-                alert(data.message);
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-    }
+  
 
     function showLoginModal() {
         var loginModal = new bootstrap.Modal(document.getElementById('loginModal'));
