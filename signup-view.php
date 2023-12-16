94b4b7beb3c1733c4d94e1ae62f033978ac5b347
NathanVassaur <125310254+NathanVassaur@users.noreply.github.com>
2023-12-15 20:29:28 -0600

    Create signup-view.php

---
+++ signup-view.php
@@ -0,0 +1,8 @@
+<?php include "view-header.php"; ?>
+
+<h1 class="mt-4">Sign Up</h1>
+<div class="container text-center">
+    <?php include "signup-newform-view.php"; ?>
+</div>
+
+<?php include "view-footer.php"; ?>
